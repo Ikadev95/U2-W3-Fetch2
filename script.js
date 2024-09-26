698abd20a64b1d6b7994691990c9878057cc34fe
Ikadev95 <fegughi@gmail.com>
2024-09-26 16:30:00 +0200

    search ok

--- script.js
+++ script.js
@@ -1,40 +1,44 @@
 const imgBtn = document.getElementById('loadImgBtn');
 const imgBtn2 = document.getElementById('loadImgBtn2');
 const HideBtn = document.getElementsByClassName('hide');
+const searchBtn = document.getElementById('search');
+const searchInput = document.getElementById('searchInput');
 
-const loadImg = function(param){
+const loadImg = function(param) {
     let imgUrl = '';
-    if(param === 1){
-        imgUrl = 'https://api.pexels.com/v1/search?query=hamsters';
+
+    // Se il parametro è una stringa fai la ricerca personalizzata
+    if (typeof param === 'string') {
+        imgUrl = `https://api.pexels.com/v1/search?query=${param}`;
     }
-    else{
+    // altrimenti sceglie tra i due : tigre e criceto
+
+    else if (param === 1) {
+        imgUrl = 'https://api.pexels.com/v1/search?query=hamsters';
+    } else {
         imgUrl = 'https://api.pexels.com/v1/search?query=tigers';
     }
-    fetch(imgUrl,{
+
+    fetch(imgUrl, {
         headers: {
             'Content-type': 'application/json',
             'Authorization': 'XZaVATWUj4uAzqhogkQQrrGqvzvhSsbtsWRTUi72XS0ZBOOE7vKjwKqp'
         }
     })
     .then((response) => {
-        if (response.ok){
+        if (response.ok) {
             console.log('ok');
             return response.json();
-        }
-        else{
-                throw new Error ('Errore della pagina');
+        } else {
+            throw new Error('Errore della pagina');
         }
     })
-    .then((page) =>{
+    .then((page) => {
         const cardsImg = document.querySelectorAll('.card > img');
-
-        /* questo per cambiare il testo dello small con l'id della card */
         const textMuted = document.querySelectorAll('small');
-        //console.log(textMuted);
-
         const photoList = page.photos;
-       // console.log(photoList)
-        for ( let i = 0; cardsImg.length > i; i++){
+
+        for (let i = 0; i < Math.min(cardsImg.length, photoList.length); i++) {
             let url = photoList[i].src.large;
             let id = photoList[i].id;
             cardsImg[i].src = url;
@@ -43,17 +47,27 @@ const loadImg = function(param){
     })
     .catch((err) => {
         console.log('errore', err);
-    })
-    
-}
+    });
+};
 
-/* questo per nascondere le card con Hide */
-
+// Event listeners per i pulsanti di caricamento immagini
 imgBtn.addEventListener('click', () => loadImg(1));
 imgBtn2.addEventListener('click', () => loadImg(2));
 
-for (let i = 0; HideBtn.length > i ; i++){
-    HideBtn[i].addEventListener('click',function(){
-        HideBtn[i].closest('.col-md-4').classList.add('d-none');
-    })
+// Event listener per nascondere le card
+for (let i = 0; i < HideBtn.length; i++) {
+    HideBtn[i].addEventListener('click', function() {
+        this.closest('.col-md-4').classList.add('d-none');
+    });
 }
+
+// Event listener per la ricerca
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    const searchString = searchInput.value;
+    if (searchString) {
+        loadImg(searchString);
+    } else {
+        alert("Inserisci un termine di ricerca valido.");
+    }
+});
